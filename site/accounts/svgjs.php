d4260d42127e99dca37dbbf34393063a1f74e9d4
wout <wout@impinc.co.uk>
2016-12-25 16:49:39 +0100

    Initial commit of new documentation/website

---
+++ site/accounts/svgjs.php
@@ -0,0 +1,16 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: svgjs
+password: >
+  $2a$10$cNheRhBTIeSy.Q8s7zzRKOKr/m0ZTrfNlBn9jRBasL43GbtrWWfLO
+email: svg.js@noreply.github.com
+language: en
+role: admin
+firstname: SVG
+lastname: JS
+history:
+  - manipulating/size
+  - manipulating/position
+  - manipulating/classes
+  - manipulating/sugar-methods
+  - manipulating/attributes
